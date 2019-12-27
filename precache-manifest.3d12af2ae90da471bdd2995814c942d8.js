2d25932fe4abeff587b8661c6d5a231027a0b2b8
Aren <aren.dalloul@colorado.edu>
2019-12-27 12:27:50 -0700

    Updates

---
+++ precache-manifest.3d12af2ae90da471bdd2995814c942d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d1190ae9f71e89e81ab59b17c706fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "449890cabe2ea1118d2d",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5d738dadbc8ca8e94a7e",
+    "url": "/static/css/main.a7b9d0c8.chunk.css"
+  },
+  {
+    "revision": "449890cabe2ea1118d2d",
+    "url": "/static/js/2.f0d44eb8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f0d44eb8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d738dadbc8ca8e94a7e",
+    "url": "/static/js/main.28702a0e.chunk.js"
+  },
+  {
+    "revision": "aa6fed8716c2be098a58",
+    "url": "/static/js/runtime-main.55a556ca.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
